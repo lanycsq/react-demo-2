68c31871158ce774b7560c32a59be4048040d4fe
lanycsq <shanqiong118@163.com>
2021-02-26 11:11:15 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state={x:1}
+
+  }
+  onClick=()=>{
+    this.setState({x:this.state.x+1})
+  }
+  render(){
+    return (
+        <div className="App">
+          <button onClick={this.onClick}>+1</button>
+          <B name={this.state.x}/>
+        </div>
+    );
+  }
+}
+class B extends React.Component{
+  componentWillReceiveProps(nextProps, nextContext) {
+    console.log("变化了")
+  }
+
+  render(){
+    return <div>{this.props.name}</div>
+  }
 }
 
 export default App;
